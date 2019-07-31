9d8a34d61fd84e712253b5c273cf1f72a675857e
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2019-07-31 15:54:12 +0200

    fetching real errors from the api

--- src/creators/issues.js
+++ src/creators/issues.js
@@ -1,62 +1,37 @@
 import {fetchIssuesBegin, fetchIssuesSuccess, fetchIssuesFailure, fetchIssueByID} from '../actions/issues';
+import API from '../config/api';
 
 export function fetchIssues() {
     return dispatch => {
         dispatch(fetchIssuesBegin());
-        const dat = [
-            {
-              "id": "1",
-              "category_id": "1",
-              "message": "TRANSFER [110] TRANSFER  Operation timed out",
-              "src_site": "WT2",
-              "dst_site": "UKI-SOUTHGRID-OX-HEP",
-              "amount": "223",
-              "failure_type": "transfer-failure",
-              "created_at": "2019-06-05 14:11:49.133219"
-            },
-            {
-              "id": "2",
-              "category_id": "2",
-              "message": "The requested service is not available at the moment.\nDetails: An unknown exception occurred.\nDetails: globus_ftp_client: the server responded with an error 530 530-globus_xio_gssapi_ftp.c:globus_l_xio_gssapi_ftp_server_read_cb:1392:  530-Server side crede",
-              "src_site": "UNKNOWN",
-              "dst_site": "ANLASC",
-              "amount": "1866",
-              "failure_type": "deletion-failure",
-              "created_at": "2019-06-05 14:11:49.337664"
+        API.get('error/12')
+        .then(res => {
+            if(res.error) {
+                throw(res.error);
             }
-          ]; 
-        dispatch(fetchIssuesSuccess(dat));
-        return dat;
+            dispatch(fetchIssuesSuccess(res.data));
+            return res.data;
+        })
+        .catch(error => {
+            dispatch(fetchIssuesFailure(error));
+        })
     }
 }
 
 export function fetchIssueById(id) {
     return dispatch => {
-        dispatch(fetchIssuesBegin());
-        const dat = [
-            {
-              "id": "1",
-              "category_id": "1",
-              "message": "TRANSFER [110] TRANSFER  Operation timed out",
-              "src_site": "WT2",
-              "dst_site": "UKI-SOUTHGRID-OX-HEP",
-              "amount": "223",
-              "failure_type": "transfer-failure",
-              "created_at": "2019-06-05 14:11:49.133219"
-            },
-            {
-              "id": "2",
-              "category_id": "2",
-              "message": "The requested service is not available at the moment.\nDetails: An unknown exception occurred.\nDetails: globus_ftp_client: the server responded with an error 530 530-globus_xio_gssapi_ftp.c:globus_l_xio_gssapi_ftp_server_read_cb:1392:  530-Server side crede",
-              "src_site": "UNKNOWN",
-              "dst_site": "ANLASC",
-              "amount": "1866",
-              "failure_type": "deletion-failure",
-              "created_at": "2019-06-05 14:11:49.337664"
-            }
-          ]; 
-        dispatch(fetchIssuesSuccess(dat));
-        dispatch(fetchIssueByID(id));
-        return id;
+      dispatch(fetchIssuesBegin());
+      API.get('error/12')
+      .then(res => {
+          if(res.error) {
+              throw(res.error);
+          }
+          dispatch(fetchIssuesSuccess(res.data));
+          dispatch(fetchIssueByID(id));
+          return id;
+      })
+      .catch(error => {
+          dispatch(fetchIssuesFailure(error));
+      })
     }
 }
