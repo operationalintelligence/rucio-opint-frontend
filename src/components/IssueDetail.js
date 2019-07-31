2fe8843229b734951e7d68eea374fa39d39c3fb9
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2019-07-31 15:55:13 +0200

    cleansup IssueDetail component

--- src/components/IssueDetail.js
+++ src/components/IssueDetail.js
@@ -8,35 +8,18 @@ class IssueDetail extends React.Component {
     constructor(props) {
         super(props);
         this.state = {issue: undefined, clicked:false}
-        this.shouldComponentRender = this.shouldComponentRender.bind(this);
     }
 
     componentDidMount() {
         const id = this.props.match.params.issue;
         this.props.dispatch(fetchIssueById(id));
+        console.log(this.props);
     }
 
-    shouldComponentRender() {
-        const {pending} = this.props.pending;
-        if(pending === false) return false;
-        return true;
-    }
 
     handleClick = () => {this.setState(() => ({ clicked:true }))}
-s
+
     render(){
-        if(!this.shouldComponentRender()) return (<div></div>)
-        const ret = (  
-            <div>
-                Possible Reason<br/>
-                Proposed actions<br/>
-                Source =>
-                Destination<br/>
-                Took Actions Button<br/>
-                <br/>
-                Share/Ticket buttons (?)<br/>
-            </div>
-    );
         return(
             this.state.clicked ? 
                 <IssueDetailFeedback issue={this.props.issue} />
