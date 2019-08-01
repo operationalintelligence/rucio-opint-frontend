c2affdd33ab8b1f29a9a2b7803963a4885bebd1e
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2019-08-01 17:48:01 +0200

    uses correct backend

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: `http://rucio-opint.web.cern.ch`
-  baseURL: `http://0.0.0.0:8088`
+  baseURL: `http://rucio-opint.web.cern.ch`
 });
