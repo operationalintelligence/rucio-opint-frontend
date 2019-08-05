e3b1fc24746a2dad5fdee85fb2773889f34a0103
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2019-08-05 10:57:24 +0200

    change port to 8080

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-const port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.listen(port, address);
 
