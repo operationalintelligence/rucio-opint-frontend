b9fd20d80bf8c4d0232590fe71d6c6df5d8d499e
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2019-08-05 10:47:59 +0200

    defines ip in server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+const port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+const address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+app.listen(port, address);
 
 console.log('App is listening on port ' + port);
